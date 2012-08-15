e5964859ba5e52f32f0b62a164a244d34d784efd
Bartosz Malkowski <bmalkowski@tigase.pl>
2012-08-15 11:14:07 +0200

    update fragment name generator

--- tigase-mobile-lib/src/org/tigase/mobile/MultiJaxmpp.java
+++ tigase-mobile-lib/src/org/tigase/mobile/MultiJaxmpp.java
@@ -70,6 +70,16 @@ public class MultiJaxmpp {
 		public boolean isRoom() {
 			return data instanceof Room;
 		}
+
+		@Override
+		public String toString() {
+			if (data instanceof Chat) {
+				return "chatid:" + ((Chat) data).getId();
+			} else if (data instanceof Room) {
+				return "roomid:" + ((Room) data).getId();
+			} else
+				return super.toString();
+		}
 	}
 
 	private final List<ChatWrapper> chats = Collections.synchronizedList(new ArrayList<ChatWrapper>());
