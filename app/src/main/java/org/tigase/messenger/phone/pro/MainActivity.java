45a2a19e781092bbddcdf9a567c7f63f2206c42a
Bartosz Malkowski <bmalkowski@tigase.pl>
2016-03-04 14:55:28 +0100

    update menu (icons, names)

--- app/src/main/java/org/tigase/messenger/phone/pro/MainActivity.java
+++ app/src/main/java/org/tigase/messenger/phone/pro/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_about) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_roster) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_chats) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_view) {
 
         }
 
